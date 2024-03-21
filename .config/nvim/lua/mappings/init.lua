b20bdde84fa376b9f30fc02533b73b0ebc221622
Noapoleon <noalegrand088@gmail.com>
2024-03-21 12:57:45 +0100

    minimal nvim config

--- .config/nvim/lua/mappings/init.lua
+++
@@ -1,7 +0,0 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
-vim.g.mapleader = ' '
-
-map('n', '<leader>n', ':NvimTreeToggle<CR>', opts)
-map('n', '<leader>f', ':NvimTreeFocus<CR>', opts)
