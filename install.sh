2f255712589c76abc04b7abb2470212f638c5b26
Noapoleon <noalegrand088@gmail.com>
2024-05-01 12:43:58 +0200

    remove && chain for tmux plugins and replaced sh with bash for plugins install script

--- install.sh
+++ install.sh
@@ -72,10 +72,10 @@ fi
 
 ### Install tmux plugins
 mkdir -p $HOME/.config/tmux/plugins &&
-git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm &&
-sh $HOME/.config/tmux/plugins/tpm/scripts/install_plugins.sh &&
+git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
+$HOME/.config/tmux/plugins/tpm/scripts/install_plugins.sh
 git clone https://github.com/jimeh/tmuxifier.git $HOME/.config/tmux/plugins/tmuxifier &&
-mkdir -p $HOME/.local/bin &&
+mkdir -p $HOME/.local/bin
 ln -s $HOME/.config/tmux/plugins/tmuxifier/bin/tmuxifier $HOME/.local/bin/tmuxifier
 
 # Goodbye
