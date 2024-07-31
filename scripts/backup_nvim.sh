ba0fac47b509eba753b07419f7b715db8d413294
Noapoleon <noalegrand088@gmail.com>
2024-07-31 14:49:15 +0200

    added nvim backup script and lazyvim installation script

---
+++ scripts/backup_nvim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
---
+++ scripts/install_lazyvim.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
